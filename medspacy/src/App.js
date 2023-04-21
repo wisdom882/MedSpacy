ae7f3181b6b9958a70ec39b96d9dcd05071a6015
king <miabiyewisdom@gmail.com>
2023-04-20 19:05:38 -0700

    ui done

--- medspacy/src/App.js
+++ medspacy/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>MedSpaCy Demo</h1>
+        <form>
+          <div className='text-area'>
+            <label for="text">Enter text:</label>
+            <textarea id="text" name="text" rows="10" cols="80"></textarea>
+          </div>
+          <br/>
+        <div>
+          <label for="file" className='upload-file'>Upload file:</label>
+          <input type="file" id="file" name="file"/>
+        </div>
+          <br/>
+
+          <input type="submit" value="Submit" className='toggle'/>
+        </form>
+        <div className='buttons'>
+          <button onclick="toggleEnt()" className='toggle'>Toggle Entity Visualization</button>
+          <button onclick="toggleDep()" className='toggle'>Toggle Dependency Visualization</button>
+          <button onclick="toggleDfs()" className='toggle'>Toggle Dataframes Table</button>
+        </div>
     </div>
   );
 }
